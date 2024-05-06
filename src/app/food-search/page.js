efca6694d8a9e6997eb81ae96b35c26a2f1ea519
Devam Patel <devamp@pdx.edu>
2024-05-06 13:00:03 -0700

    setup next project, added sample routes and layouts

---
+++ src/app/food-search/page.js
@@ -0,0 +1,3 @@
+export default function FoodSearch() {
+  return <h1>Food Search Page Route</h1>;
+}
---
+++ src/app/meal-tracking/page.js
@@ -0,0 +1,3 @@
+export default function FoodSearch() {
+  return <h1>Meal Tracking Page Route</h1>;
+}
